0232b2b96c3409f1eac0ed28c7d71604c8ccac7e
Ryan Tucker <rtucker@gmail.com>
2011-06-12 13:17:51 -0400

    linodestats: catch XML-related exceptions
    This helps prevent visible problems when the plugin breaks.
    
    Related: https://github.com/rtucker/sycamore/issues/12

--- Sycamore/macro/linodestats.py
+++ Sycamore/macro/linodestats.py
@@ -35,34 +35,38 @@ def execute(macro, args, formatter=None):
             try:
                 statsfd = urllib.urlopen(xmlurl)
                 xmltree = xml.dom.minidom.parseString(statsfd.read()).childNodes[0]
+                # make sure we have useful stuff here
+                host = xmltree.getElementsByTagName('host')[0]
             except:
                 # it's missing
                 return formatter.rawHTML('<strong>VPS Stats</strong>: Unavailable (XML error)')
             mc.set(mc_key, xmltree, time=600)
 
         # messy XML parsing action
-        host = xmltree.getElementsByTagName('host')[0]
-        upsince = xmltree.getElementsByTagName('upSince')[0]
-        bwdata = xmltree.getElementsByTagName('bwdata')[0]
-        request = xmltree.getElementsByTagName('request')[0]
-        stats['host'] = host.getElementsByTagName('host')[0].childNodes[0].wholeText
-        stats['pendingJobs'] = int(host.getElementsByTagName('pendingJobs')[0].childNodes[0].wholeText)
-        if stats['pendingJobs'] == 1:
-            stats['pJs'] = ''
-        else:
-            stats['pJs'] = 's'
-        stats['upSince'] = upsince.childNodes[0].wholeText
-        stats['total_bytes'] = int(bwdata.getElementsByTagName('total_bytes')[0].childNodes[0].wholeText)
-        stats['total_gb'] = float(stats['total_bytes'])/1024/1024/1024
-        stats['max_avail'] = int(bwdata.getElementsByTagName('max_avail')[0].childNodes[0].wholeText)
-        stats['rx_bytes'] = int(bwdata.getElementsByTagName('rx_bytes')[0].childNodes[0].wholeText)
-        stats['tx_bytes'] = int(bwdata.getElementsByTagName('tx_bytes')[0].childNodes[0].wholeText)
-        stats['tx_perc'] = float(stats['tx_bytes'])/float(stats['total_bytes'])*100
-        stats['rx_perc'] = float(stats['rx_bytes'])/float(stats['total_bytes'])*100
-        stats['bw_perc'] = float(stats['total_bytes'])/float(stats['max_avail'])*100
-        stats['DateTimeStamp'] = request.getElementsByTagName('DateTimeStamp')[0].childNodes[0].wholeText
-        loadavg = string.split(open('/proc/loadavg', 'r').readline())
-        stats['loadavg'] = '%s %s %s' % (loadavg[0], loadavg[1], loadavg[2])
+        try:
+            upsince = xmltree.getElementsByTagName('upSince')[0]
+            bwdata = xmltree.getElementsByTagName('bwdata')[0]
+            request = xmltree.getElementsByTagName('request')[0]
+            stats['host'] = host.getElementsByTagName('host')[0].childNodes[0].wholeText
+            stats['pendingJobs'] = int(host.getElementsByTagName('pendingJobs')[0].childNodes[0].wholeText)
+            if stats['pendingJobs'] == 1:
+                stats['pJs'] = ''
+            else:
+                stats['pJs'] = 's'
+            stats['upSince'] = upsince.childNodes[0].wholeText
+            stats['total_bytes'] = int(bwdata.getElementsByTagName('total_bytes')[0].childNodes[0].wholeText)
+            stats['total_gb'] = float(stats['total_bytes'])/1024/1024/1024
+            stats['max_avail'] = int(bwdata.getElementsByTagName('max_avail')[0].childNodes[0].wholeText)
+            stats['rx_bytes'] = int(bwdata.getElementsByTagName('rx_bytes')[0].childNodes[0].wholeText)
+            stats['tx_bytes'] = int(bwdata.getElementsByTagName('tx_bytes')[0].childNodes[0].wholeText)
+            stats['tx_perc'] = float(stats['tx_bytes'])/float(stats['total_bytes'])*100
+            stats['rx_perc'] = float(stats['rx_bytes'])/float(stats['total_bytes'])*100
+            stats['bw_perc'] = float(stats['total_bytes'])/float(stats['max_avail'])*100
+            stats['DateTimeStamp'] = request.getElementsByTagName('DateTimeStamp')[0].childNodes[0].wholeText
+            loadavg = string.split(open('/proc/loadavg', 'r').readline())
+            stats['loadavg'] = '%s %s %s' % (loadavg[0], loadavg[1], loadavg[2])
+        except:
+            return formatter.rawHTML('<strong>VPS Stats</strong>: Unavailable (XML element parsing error)')
 
         return formatter.rawHTML("""
           <strong>VPS Stats</strong>:<br>&nbsp;&nbsp;&nbsp;&nbsp;
